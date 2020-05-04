450dec9360b8a73e3faee5d59b2b4c693835fa1f
RikBala <rik280491@googlemail.com>
2020-05-04 18:41:33 +0100

    able to create playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,32 @@
 class PlaylistsController < ApplicationController
+    def index 
+        playlists = Playlist.all 
+        render json: playlists
+    end 
+    
+    def show 
+        playlist = Playlist.find(params[:id])
+        render json: playlist
+    end
+    
+    def create
+        
+        playlist = Playlist.create(playlist_params)
+        render json: playlist
+    end 
+
+    def destroy
+        playlist = Playlist.find(params[:id])
+        playlist.destroy
+        render json: playlist
+    end 
+
+    private 
+
+    def playlist_params 
+        params.require(:playlist).permit(:name, :user_id)
+
+    end 
+
+
 end
